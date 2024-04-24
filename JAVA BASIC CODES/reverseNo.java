ee14180813c7612b54c2880c77186a3c2fda4dfb
krushi-19 <71973292+krushi-19@users.noreply.github.com>
2024-04-25 02:46:37 +0530

    Update reverseNo.java

--- JAVA BASIC CODES/reverseNo.java
+++ JAVA BASIC CODES/reverseNo.java
@@ -11,6 +11,28 @@ static int rev=0;
  int rem = n%10;
   rev = rev*10+rem;
    rev1(n/10);
+
+  int no = 5432;
+   LOGIC;- DIVIDE NO BY 10 
+    we will get quotient and rem then again divide by 10 
+    int rev = 0
+    rev = rev*10 + rem
+       0*10+2 = 2  
+       2*10+3= 23
+       23*10+4 = 234
+      234*10+5 = 2345
+
+    int no = 5432, rem = 0, rev=0;
+   while(no != 0){
+    rem = no%10;
+    rev = rev*10+rem;
+    no = no / 10 ....> quotient provide karega. 
+   }
+   sop(rev);S
+
+    
+
+    
  
 
 
